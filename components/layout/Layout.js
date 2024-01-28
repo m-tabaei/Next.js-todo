aa44662202366e84ba867bb90f73876a40c740d8
mtabaie <mtabaie@gmail.com>
2024-01-28 19:07:39 +0330

    update

---
+++ components/layout/Layout.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { VscListSelection } from "react-icons/vsc";
+import { BiMessageSquareAdd } from "react-icons/bi";
+import { RxDashboard } from "react-icons/rx";
+import { FiLogOut } from "react-icons/fi";
+
+function Layout({ children }) {
+
+  const logOutHandler = () => {
+    signOut();
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <p>Botostart Todo App</p>
+        {status === "authenticated" ? (
+          <button onClick={logOutHandler}>
+            Logout
+            <FiLogOut />
+          </button>
+        ) : null}
+      </header>
+      <div className="container--main">
+        <aside>
+          <p>Welcome 👋</p>
+          <ul>
+            <li>
+              <VscListSelection />
+              <Link href="/">Todos</Link>
+            </li>
+            <li>
+              <BiMessageSquareAdd />
+              <Link href="/add-todo">Add Todo</Link>
+            </li>
+            <li>
+              <RxDashboard />
+              <Link href="/profile">Profile</Link>
+            </li>
+          </ul>
+        </aside>
+        <section>{children}</section>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
